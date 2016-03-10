689ce39440ba0335bd1f19dd7a618d6909eca93f
nexemjail <nexemjail@gmail.com>
2016-03-11 02:06:54 +0300

    resolved all

--- socket_server.py
+++ socket_server.py
@@ -1,20 +1,8 @@
 from host_data import HOST, PORT
 import socket
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-import SocketServer
-=======
->>>>>>> master
-
-
-<<<<<<< HEAD
-=======
 from pickler import unpickle
+
 BUFFER_SIZE = 1024
->>>>>>> Stashed changes
-=======
-BUFFER_SIZE = 1024
->>>>>>> master
 
 
 def _serve():
@@ -23,13 +11,6 @@ def _serve():
     server_socket.listen(5)
     while True:
         client_socket, address = server_socket.accept()
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-        client_socket.send(SUCCSESS_MESSAGE)
-
-if __name__ == '__main__':
-    serve_responses()
-=======
         input_data = []
         while True:
             data = client_socket.recv(BUFFER_SIZE)
@@ -48,19 +29,3 @@ def run_server():
 
 if __name__ == '__main__':
     run_server()
->>>>>>> Stashed changes
-=======
-        input_data = []
-        data = client_socket.recv(BUFFER_SIZE)
-        if not data:
-            full_data = ''.join(input_data)
-            print full_data
-            del input_data
-            del data
-            break
-        input_data.append(data)
-
-
-def run_server():
-    _serve()
->>>>>>> master
--- worker.py
+++ worker.py
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 class Worker(object):
     pass
-=======
-from pickler import pickle_object, pickle_object_to_file
-
-class Worker(object):
->>>>>>> master
